29161dea1d10ff1b3240e9d73a5d6a8535cc2726
FxIvan <almendraivan210814@gmail.com>
2022-01-17 21:29:46 -0300

    agregando GIF para el readme

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.set('port', process.env.PORT || 4000)
+//app.set('port', process.env.PORT || 3000)
 
 app.use(cors())
 app.use(express.json())
--- backend/src/index.js
+++ backend/src/index.js
@@ -3,7 +3,7 @@ const app = require('./app')
 require('./database')
 
 async function Main(){
-    await app.listen(app.get('port'))
+    await app.listen(process.env.PORT || 3000)
     console.log("Esuchando por el Puerto:" , app.get('port'))
 }
 
